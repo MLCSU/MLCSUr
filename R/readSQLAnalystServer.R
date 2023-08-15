d770b04205f95f441c198b7409ae8d48f715ba18
ScottJohnson2 <99869951+ScottJohnson2@users.noreply.github.com>
2023-08-15 15:58:58 +0100

    Add initial dcumentation

--- R/readSQLAnalystServer.R
+++ R/readSQLAnalystServer.R
@@ -1,17 +1,41 @@
-# Read data from Midlands and Lancashire CSU Business Intelligence Analyst SQL Server
-#
-# This function allows the user to connect to the commonly used analyst server directly to access data
-#
-# The only input needed is the SQL query used to generate the data
-#
-# the following block ised used to automatically manage
+
+# the following block i used used to automatically manage
 # roxygen namespace tags
+#' Read data from Midlands and Lancashire CSU Analyst SQL Server
+#'
+#' Read Data from Midlands and Lancashire CSU Business Intelligence Analyst SQL Server
+#'
+#' This function allows the user to connect to the commonly used analyst server directly to access data
+#'
+#' The only input needed is the SQL query used to generate the data
+#'
 #' @importFrom DBI dbConnect
 #' @importFrom odbc odbc
-#' @export
+#' @export readSQLAnalystServer
+#'
+#'
 
 #define function name and input variable
+#' Title
+#'
+#' @param SQLQuery A SQL Query to read data from the Midlands and Lancashire CSU Analyst Server.
+#'
+#' @return A dataset taken from Midlands and Lancashire CSU SQL Analyst Server
+#'
+#'
+#' @examples
+#'
+#' library(MLCSUr)
+#'
+#' # write a SQL query to import the data you require
+#' exampleSQLQuery  <- "SELECT TOP (1000) * FROM EAT_Reporting.dbo.tbInpatientEpisodes"
+#'
+#' # pass the SQL query to the readSQLAnalystServer function to read the data in
+#' episodesData <- readSQLAnalystServer(exampleSQLQuery)
+#'
+
 readSQLAnalystServer <- function(SQLQuery) {
+
 
 
 #create a connection to MLCSU analyst server using user credentials
